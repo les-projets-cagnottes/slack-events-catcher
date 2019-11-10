4aefb65a101307939990780e8e30f3f2d309e59d
Thomah <5090230+Thomah@users.noreply.github.com>
2019-11-10 21:18:41 +0100

    Playing with core API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 require('dotenv').config();
 
 const { createEventAdapter } = require('@slack/events-api');
+const cron = require("node-cron");
+const request = require('request');
 const logger = require('./logger.js');
 
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 
+var coreApiToken = process.env.CORE_API_TOKEN;
+
+// Maintain a token with the core
+cron.schedule("* * * * *", function () {
+    logger.log("Refresh core API Token");
+    const options = {
+        url: `${process.env.CORE_API_URL}/auth/refresh`,
+        headers: {
+            'Authorization': `Bearer ${coreApiToken}`
+        }
+    };
+    request(options, (err, res, body) => {
+        if (err) { return console.log(err); }
+        coreApiToken = JSON.parse(body).token;
+    });
+});
+
+
+
+
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('message', (event) => {
-    logger.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+    logger.log(`Received an event : ${JSON.stringify(event)}`);
+});
+
+slackEvents.on('team_join', (event) => {
+    logger.log(`Received an event : ${JSON.stringify(event)}`);
 });
 
 (async () => {
