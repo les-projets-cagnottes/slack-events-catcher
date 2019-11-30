f3a1452aa2c9f16c1423a49c67624fdff035ad46
Thomah <5090230+Thomah@users.noreply.github.com>
2019-11-30 17:11:05 +0100

    Fix production deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $SCRIPTPATH
+cd $SCRIPTPATH/..
 echo "$(date -u) Automatic Deploy"  >> ./console.log
-./slack-events-catcher.sh stop
-cd ..
-npm i
-./slack-events-catcher.sh start
+bin/slack-events-catcher.sh stop
+sleep 1
+bin/slack-events-catcher.sh start
