912ece400f3ec3635f273f28e6e98596a862281c
Thomah <5090230+Thomah@users.noreply.github.com>
2019-11-30 16:46:50 +0100

    Fix path to binary in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,6 @@
 
 cd $1
 echo "$(date -u) Automatic Deploy"  >> ./console.log
-bin/slack-events-catcher.sh stop
+./slack-events-catcher.sh stop
 sleep 1
-bin/slack-events-catcher.sh start
+./slack-events-catcher.sh start
