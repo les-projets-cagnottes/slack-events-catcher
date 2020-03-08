eeeae7ca2e132e683d15b01e206436b726183f66
Thomah <5090230+Thomah@users.noreply.github.com>
2020-03-08 16:42:53 +0100

    Fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,17 +2,19 @@
 
 TAG_NAME=$1
 DEPLOY_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"/..
-FINAL_LOCATION=/opt/les-projets-cagnottes/core/current
+FINAL_LOCATION=/opt/les-projets-cagnottes/slack-events-catcher
 
 echo "$(date -u) Automatic Deploy"  >> ./console.log
 
 sudo service les-projets-cagnottes-slack stop
 sleep 1
 
-mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
+mkdir -p ${FINAL_LOCATION}/current
 sudo cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-slack.service /etc/systemd/system/les-projets-cagnottes-slack.service
-cp ${DEPLOY_LOCATION}/bin/setenv.sh.template ${FINAL_LOCATION}/setenv.template
-cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-slack.sh ${FINAL_LOCATION}/les-projets-cagnottes-slack.sh
+cp ${FINAL_LOCATION}/current/bin/setenv.sh ${DEPLOY_LOCATION}/bin/setenv.sh
+cp -R ${DEPLOY_LOCATION} ${FINAL_LOCATION}/current
+
+mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
 cp -R ${DEPLOY_LOCATION} ${FINAL_LOCATION}/${TAG_NAME}
 
 sudo systemctl daemon-reload
