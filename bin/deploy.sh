c553bde23dfa42e231527efedc50215b16913d84
Thomah <5090230+Thomah@users.noreply.github.com>
2020-03-08 16:15:43 +0100

    Rework release workflow

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $SCRIPTPATH/..
+TAG_NAME=$1
+DEPLOY_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"/..
+FINAL_LOCATION=/opt/les-projets-cagnottes/core
+
 echo "$(date -u) Automatic Deploy"  >> ./console.log
-sudo service slack-events-catcher stop
+
+sudo service les-projets-cagnottes-slack stop
 sleep 1
-sudo service slack-events-catcher start
+
+mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
+sudo cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-slack.service /etc/systemd/system/les-projets-cagnottes-slack.service
+cp ${DEPLOY_LOCATION}/bin/setenv.sh.template ${FINAL_LOCATION}/setenv.template
+cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-slack.sh ${FINAL_LOCATION}/les-projets-cagnottes-slack.sh
+cp -R ${DEPLOY_LOCATION} ${FINAL_LOCATION}/${TAG_NAME}
+
+sudo systemctl daemon-reload
+sudo service les-projets-cagnottes-slack start
 
 exit 0
