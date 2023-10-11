27490036a0424f0699c7d3dbaaec037680c04d1d
Thomah <5090230+Thomah@users.noreply.github.com>
2023-10-11 21:26:53 +0200

    feat: add properties for voting with Slack

--- src/interactive.js
+++ src/interactive.js
@@ -7,27 +7,31 @@ var run = function (coreApiToken, payload) {
             var slackTeamId = payload.team.id;
             var slackUserId = payload.user.id;
             var voteType = action.action_id.replace("vote_", "");
-            vote(coreApiToken, slackTeamId, slackUserId, voteType.toUpperCase(), parseInt(action.value));
+            var notifIdAndProjectId = action.value.split("_");
+            slackVote = {
+                slackNotificationId: parseInt(notifIdAndProjectId[0]),
+                messageTs: payload.message.ts,
+                type: voteType.toUpperCase(),
+                projectId: parseInt(notifIdAndProjectId[1]),
+                slackUserId: slackUserId
+            };
+            vote(coreApiToken, slackTeamId, slackVote);
         }
     });
 }
 
-var vote = function (coreApiToken, slackTeamId, slackUserId, voteType, projectId) {
+var vote = function (coreApiToken, slackTeamId, slackVote) {
     const options = {
         method: 'POST',
         url: `${process.env.LPC_CORE_API_URL}/api/slack/${slackTeamId}/vote`,
-        body: JSON.stringify({
-            type: voteType,
-            projectId: projectId,
-            slackUserId: slackUserId
-        }),
+        body: JSON.stringify(slackVote),
         headers: {
             'Authorization': `Bearer ${coreApiToken}`,
             'Content-Type': 'application/json'
         }
     };
     request(options, (err, res) => {
-        if (err) { logger.log(err); } else { logger.log(`Sending vote ${voteType} for ${projectId} in team ${slackTeamId} : ${res.statusCode}`) }
+        if (err) { logger.log(err); } else { logger.log(`Sending vote in team ${slackTeamId} : ${res.statusCode}`) }
     });
 }
 
